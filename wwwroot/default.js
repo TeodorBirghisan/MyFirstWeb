a526077bd3c3fba84757f0b18a5c1759db1a904c
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-03-22 13:45:23 +0200

    deleted 2 useless js lines

--- wwwroot/default.js
+++ wwwroot/default.js
@@ -1,6 +1,4 @@
 ﻿function addNewcomer() {
-    var list = document.getElementById("list");
-    var newcomer = document.getElementById("newcomer");
     var node = document.createElement("li");
     node.innerText = newcomer.value;
     list.appendChild(node);
