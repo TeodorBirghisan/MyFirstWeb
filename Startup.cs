c3a8a2b1d5019ad0b64aab5d921e8fe67ce68723
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-03-22 13:16:18 +0200

    switched app. functions

--- Startup.cs
+++ Startup.cs
@@ -21,8 +21,8 @@ namespace MyFirstWeb
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseStaticFiles();
             app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
